bed3acf47867be6df9eed7a2931bca35bc99c9f2
TtroFX <ttro0419@gmail.com>
2026-08-25 19:09:38 +0900

    Precache complete practice navigation

--- app/sw.js
+++ app/sw.js
@@ -8,7 +8,7 @@ const LEGACY_PRIVATE_CACHE='mts-private-production-v1';
 const VERSION_PATH='./pwa-version.json';
 const OFFLINE_PATH='./offline.html';
 const SHELL_ASSETS=[
-  './index.html','./p5.css','./p5_app.js','./reader_sheet.js','./p6_private_data.js','./pages_private_import.js','./p6_pwa.css','./p6_pwa.js',
+  './index.html','./p5.css','./p5_app.js','./practice_navigation.js','./reader_sheet.js','./p6_private_data.js','./pages_private_import.js','./p6_pwa.css','./p6_pwa.js',
   './P2_learning.html','./008_cue_practice_P3.html','./009_rehearsal_P4.html','./mousetrap_line_structure.json','./manifest.webmanifest',VERSION_PATH,OFFLINE_PATH,
   './icons/icon-192.png','./icons/icon-512.png','./icons/icon-maskable-512.png'
 ];
